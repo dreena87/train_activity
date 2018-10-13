49d9efb1884e81b52f87cbb1ee8397ad02bf34fe
Adreena Howard <adreena.howard@yahoo.com>
2018-10-12 20:50:30 -0500

    added css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,17 +14,17 @@ var database = firebase.database();
 
 var employeeDir = database.ref("/employees");
   
-  //click event when submit is pressed
+  
   $(".btn-secondary").on("click",function(e){
     console.log("clicked");
     e.preventDefault();
-    //store information from fields into vars
+    
     var name = $("#train-name").val().trim();
     var destination = $("#destination-name").val().trim();
     var time = $("#time-name").val().trim(); 
     var frequency = $("#frequency-name").val().trim();
 
-    //lets log to verify data is good
+    
     console.log(name,destination,time,frequency);
 
     employeeDir.push({
@@ -34,14 +34,14 @@ var employeeDir = database.ref("/employees");
       frequency: frequency,
     });
 
-    $(".arrival-form").trigger("reset");//clear form
+    $(".arrival-form").trigger("reset");
 
   });
 
-  //if there is something in the database add to the train table
+  
   employeeDir.on("child_added", function(childSnapshot) {
 
-    // Log everything that's coming out of snapshot
+    
     console.log(childSnapshot.val().name);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().time);
@@ -52,7 +52,7 @@ var employeeDir = database.ref("/employees");
     var time = childSnapshot.val().time;
     var frequency = childSnapshot.val().frequency;
 
-    //let's create a train object and append to the schedule
+    
     var newRow = $("<tr>");
     var nameDisplay = $("<td>");
     var destinationDisplay = $("<td>");
@@ -71,12 +71,12 @@ var employeeDir = database.ref("/employees");
     minutesDisplay.attr("scope","col");
     removeDisplay.attr("scope","col");
 
-    //calculate difference of first train time minus current time modules the frequency between trains
+    
     var minutes = frequency - Math.floor(((moment().unix("X")-moment(time, "hh:mm").unix("X"))/60)%frequency);
     console.log("Delta time; " + (moment().unix("X")-moment(time, "hh:mm").unix("X"))/60);
     console.log("until next train: "+ ((moment().unix("X")-moment(time, "hh:mm").unix("X"))/60)%frequency)
 
-    //now time of next train is current time + minutes
+    
     var timeNext = moment().add(minutes,'m').format('HH:mm');
 
     nameDisplay.html(name);
@@ -95,19 +95,19 @@ var employeeDir = database.ref("/employees");
     console.log("Errors handled: " + errorObject.code);
   });
 
-  //create clock using moment.js to display to user and also update the train schedule (per minute)
+  
 
   function rollingClock() {
-    var clock = moment().format('HH:mm:ss');  //create clock with hours, minutes and seconds format
-    $("#current-time").html(clock); //append time to current-time element
-    setTimeout(rollingClock,1000);  //callback every second;
+    var clock = moment().format('HH:mm:ss');  
+    $("#current-time").html(clock);
+    setTimeout(rollingClock,1000);  
     
   }
 
-  //create on event when remove button is pressed for row to remove the entire row from the database
+  
   $(".table").on("click",".btn-danger",function(f){
 
-    f.preventDefault(); //don't refresh window
+    f.preventDefault(); 
     console.log("I detected a press!");
     var trainName = $(this).closest('tr').find("td:first-child").text();
     console.log(trainName);
@@ -116,19 +116,19 @@ var employeeDir = database.ref("/employees");
       snapshot.forEach(function(snapshot1) {
           console.log(snapshot1.key);
           console.log(snapshot1.val().name);
-          if (snapshot1.val().name == trainName) {  //does child content match train clicked?
+          if (snapshot1.val().name == trainName) {  
 
             const key = snapshot1.key;
             console.log("key found: "+ key);
 
-            // delete stickynote node from firebase
+            
             employeeDir.child(key).remove();
           }
       });
     });
-    $(this).closest('tr').remove(); //delete closest tr to button
+    $(this).closest('tr').remove();
 
   });
 
-  //initialize timer
+  
   rollingClock();
